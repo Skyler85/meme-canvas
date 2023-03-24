a5a78a1e25969f6fffac4c4b7d7f3995fac5f77b
Skyler <worstlie1@gmail.com>
2023-03-24 21:59:01 +0900

    canvas Update

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.fillRect(50, 50, 100, 200);
